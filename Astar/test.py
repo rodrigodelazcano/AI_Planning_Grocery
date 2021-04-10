e343d0f95a89f6914183f5fcc730ac40472dc36d
Rodrigo de Lazcano <r.l.p.v96@gmail.com>
2021-04-10 13:23:56 -0400

    add world models

--- Astar/test.py
+++ Astar/test.py
@@ -2,7 +2,7 @@ import numpy as np
 from map import Map
 from products import Products
 
-map = Map()
+map = Map(radius=0.177)
 
 collision = map.check_collision(15, 15)
 
@@ -15,3 +15,9 @@ print(new_list)
 new_list1 = prod.get_random_list()
 print(new_list1)
 print(collision)
+
+for product, coord in prod.product_list.items():
+
+    collision = map.check_collision(coord[0],coord[1])
+    if (collision == True):
+        print(product, " collides")
