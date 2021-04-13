7e9f22678f4f1dc09d93831acc024c6e1a5c9f7f
Naman Gupta <naman@173-17-0-30.client.mchsi.com>
2021-04-13 14:01:07 -0400

    Updated groceryplan.py
    Added methods and actions

--- grocery_ai_planning_ws/src/ai_planning/script/groceryplan.py
+++ grocery_ai_planning_ws/src/ai_planning/script/groceryplan.py
@@ -1,6 +1,7 @@
 from pyhop2 import pyhop2
 import products
 import gbfs
+from astar import astar
 
 
 domain_name = 'groceryplan'
@@ -19,7 +20,7 @@ rigid = pyhop2.State('rigid relations')
 # These types are used by the 'is_a' helper function, later in this file
 rigid.types = {
     'person':   ['robot'],
-    'location': grocery_list}
+    'location': ['item']}
 # rigid.dist = {
 #     ('home_a', 'park'):8,    ('home_b', 'park'):2, 
 #     ('station', 'home_a'):1, ('station', 'home_b'):7,
@@ -28,10 +29,11 @@ rigid.types = {
 # prototypical initial state
 state0 = pyhop2.State()
 state0.loc = {'robot':(1,1)}
+state0.wayp = []
 
-while grocery_list:
-	item = grocery_list.pop()
-	state0.loc[item] = prod.product_list[item]
+# while grocery_list:
+# 	item = grocery_list.pop()
+# 	state0.loc[item] = prod.product_list[item]
 
 
 
@@ -56,38 +58,39 @@ def is_a(variable,type):
 ###############################################################################
 # Actions:
 
-def walk(state,p,x,y):
-    if is_a(p,'person') and is_a(x,'location') and is_a(y,'location') and x != y:
-        if state.loc[p] == x:
-            state.loc[p] = y
+def move_robot(state,r,x,y):
+    if is_a(r,'robot') and is_a(x,'location') and is_a(y,'location') and x != y:
+        if state.loc[r] == x:
+            state.wayp = astar(x,y)
+            state.loc[r] = y
             return state
 
-def call_taxi(state,p,x):
-    if is_a(p,'person') and is_a(x,'location'):
-        state.loc['taxi1'] = x
-        state.loc[p] = 'taxi1'
-        return state
+# def call_taxi(state,p,x):
+#     if is_a(p,'person') and is_a(x,'location'):
+#         state.loc['taxi1'] = x
+#         state.loc[p] = 'taxi1'
+#         return state
     
-def ride_taxi(state,p,y):
-    # if p is a person, p is in a taxi, and y is a location:
-    if is_a(p,'person') and is_a(state.loc[p],'taxi') and is_a(y,'location'):
-        taxi = state.loc[p]
-        x = state.loc[taxi]
-        if is_a(x,'location') and x != y:
-            state.loc[taxi] = y
-            state.owe[p] = taxi_rate(distance(x,y))
-            return state
+# def ride_taxi(state,p,y):
+#     # if p is a person, p is in a taxi, and y is a location:
+#     if is_a(p,'person') and is_a(state.loc[p],'taxi') and is_a(y,'location'):
+#         taxi = state.loc[p]
+#         x = state.loc[taxi]
+#         if is_a(x,'location') and x != y:
+#             state.loc[taxi] = y
+#             state.owe[p] = taxi_rate(distance(x,y))
+#             return state
 
-def pay_driver(state,p,y):
-    if is_a(p,'person'):
-        if state.cash[p] >= state.owe[p]:
-            state.cash[p] = state.cash[p] - state.owe[p]
-            state.owe[p] = 0
-            state.loc[p] = y
-            return state
+# def pay_driver(state,p,y):
+#     if is_a(p,'person'):
+#         if state.cash[p] >= state.owe[p]:
+#             state.cash[p] = state.cash[p] - state.owe[p]
+#             state.owe[p] = 0
+#             state.loc[p] = y
+#             return state
 
 
-pyhop2.declare_actions(walk, call_taxi, ride_taxi, pay_driver)
+pyhop2.declare_actions(move_robot)
 
 
 ###############################################################################
@@ -95,68 +98,68 @@ pyhop2.declare_actions(walk, call_taxi, ride_taxi, pay_driver)
 
 
 # this does the same thing as the action model
-def c_walk(state,p,x,y):
-    if is_a(p,'person') and is_a(x,'location') and is_a(y,'location'):
-        if state.loc[p] == x:
-            state.loc[p] = y
-            return state
+# def c_walk(state,p,x,y):
+#     if is_a(p,'person') and is_a(x,'location') and is_a(y,'location'):
+#         if state.loc[p] == x:
+#             state.loc[p] = y
+#             return state
 
 
-# c_call_taxi, version used in simple_tasks1
-# this is like the action model except that the taxi doesn't always arrive
-def c_call_taxi(state,p,x):
-    if is_a(p,'person') and is_a(x,'location'):
-        if random.randrange(2) > 0:
-            state.loc['taxi1'] = x
-            state.loc[p] = 'taxi1'
-            print('Action> c_call_taxi succeeded.  This happens with Pr = 1/2.')
-            return state
-        else:
-            print('Action> c_call_taxi failed.  This happens with Pr = 1/2.')
+# # c_call_taxi, version used in simple_tasks1
+# # this is like the action model except that the taxi doesn't always arrive
+# def c_call_taxi(state,p,x):
+#     if is_a(p,'person') and is_a(x,'location'):
+#         if random.randrange(2) > 0:
+#             state.loc['taxi1'] = x
+#             state.loc[p] = 'taxi1'
+#             print('Action> c_call_taxi succeeded.  This happens with Pr = 1/2.')
+#             return state
+#         else:
+#             print('Action> c_call_taxi failed.  This happens with Pr = 1/2.')
 
 
-# c_ride_taxi, version used in simple_tasks1
-# this does the same thing as the action model
-def c_ride_taxi(state,p,y):
-    # if p is a person, p is in a taxi, and y is a location:
-    if is_a(p,'person') and is_a(state.loc[p],'taxi') and is_a(y,'location'):
-        taxi = state.loc[p]
-        x = state.loc[taxi]
-        if is_a(x,'location') and x != y:
-            state.loc[taxi] = y
-            state.owe[p] = taxi_rate(distance(x,y))
-            return state
+# # c_ride_taxi, version used in simple_tasks1
+# # this does the same thing as the action model
+# def c_ride_taxi(state,p,y):
+#     # if p is a person, p is in a taxi, and y is a location:
+#     if is_a(p,'person') and is_a(state.loc[p],'taxi') and is_a(y,'location'):
+#         taxi = state.loc[p]
+#         x = state.loc[taxi]
+#         if is_a(x,'location') and x != y:
+#             state.loc[taxi] = y
+#             state.owe[p] = taxi_rate(distance(x,y))
+#             return state
 
 
-# this does the same thing as the action model
-def c_pay_driver(state,p,y):
-    return pay_driver(state,p,y)
+# # this does the same thing as the action model
+# def c_pay_driver(state,p,y):
+#     return pay_driver(state,p,y)
 
 
-pyhop2.declare_commands(c_walk, c_call_taxi, c_ride_taxi, c_pay_driver)
+# pyhop2.declare_commands(c_walk, c_call_taxi, c_ride_taxi, c_pay_driver)
 
 ###############################################################################
 # Methods:
 
-def do_nothing(state,p,y):
-    if is_a(p,'person') and is_a(y,'location'):
-        x = state.loc[p]
-        if x == y:
-            return []
+# def do_nothing(state,p,y):
+#     if is_a(p,'person') and is_a(y,'location'):
+#         x = state.loc[p]
+#         if x == y:
+#             return []
 
-def travel_by_foot(state,p,y):
-    if is_a(p,'person') and is_a(y,'location'):
-        x = state.loc[p]
-        if x != y and distance(x,y) <= 2:
-            return [('walk',p,x,y)]
+def shop(state,r,y):
+    if is_a(r,'robot') and is_a(y,'location'):
+        x = state.loc[r]
+        if x != y:
+            return [('move_robot',r,x,y)]
 
-def travel_by_taxi(state,p,y):
-    if is_a(p,'person') and is_a(y,'location'):
-        x = state.loc[p]
-        if x != y and state.cash[p] >= taxi_rate(distance(x,y)):
-            return [('call_taxi',p,x), ('ride_taxi',p,y), ('pay_driver',p,y)]
+# def travel_by_taxi(state,p,y):
+#     if is_a(p,'person') and is_a(y,'location'):
+#         x = state.loc[p]
+#         if x != y and state.cash[p] >= taxi_rate(distance(x,y)):
+#             return [('call_taxi',p,x), ('ride_taxi',p,y), ('pay_driver',p,y)]
 
-pyhop2.declare_task_methods('travel',do_nothing,travel_by_foot,travel_by_taxi)
+pyhop2.declare_task_methods('groceryshop',shop)
 
 
 ###############################################################################
@@ -185,57 +188,57 @@ We'll do it several times with different values for 'verbose'.
 """)
 
 
-    expected = [('call_taxi', 'alice', 'home_a'), ('ride_taxi', 'alice', 'park'), ('pay_driver', 'alice', 'park')]
+    expected = [('move_robot', 'robot', 'item')]
 
-    print("-- If verbose=0, the planner will return the solution but print nothing.")
-    result = pyhop2.find_plan(state1,[('travel','alice','park')],verbose=0)
-    check_result(result,expected)
+    # print("-- If verbose=0, the planner will return the solution but print nothing.")
+    # result = pyhop2.find_plan(state1,[('travel','alice','park')],verbose=0)
+    # check_result(result,expected)
 
-    print("-- If verbose=1, the planner will print the problem and solution,")
-    print("-- and then return the solution.\n")
-    result = pyhop2.find_plan(state1,[('travel','alice','park')],verbose=1)
-    check_result(result,expected)
+    # print("-- If verbose=1, the planner will print the problem and solution,")
+    # print("-- and then return the solution.\n")
+    # result = pyhop2.find_plan(state1,[('travel','alice','park')],verbose=1)
+    # check_result(result,expected)
 
-    print("-- If verbose=2, the planner will print the problem, a note at each")
-    print("-- recursive call, and the solution. Then it will return the solution.\n")
-    result = pyhop2.find_plan(state1,[('travel','alice','park')],verbose=2)
-    check_result(result,expected)
-    pause()
+    # print("-- If verbose=2, the planner will print the problem, a note at each")
+    # print("-- recursive call, and the solution. Then it will return the solution.\n")
+    # result = pyhop2.find_plan(state1,[('travel','alice','park')],verbose=2)
+    # check_result(result,expected)
+    # pause()
 
     print("-- If verbose=3, the planner will print even more information.\n")
-    result = pyhop2.find_plan(state1,[('travel','alice','park')],verbose=3)
+    result = pyhop2.find_plan(state1,[('groceryshop','robot','item')],verbose=3)
     check_result(result,expected)
 
     pause()
-    print("""
-Find a plan that will first get Alice to the park, then get Bob to the park.
-""")
+#     print("""
+# Find a plan that will first get Alice to the park, then get Bob to the park.
+# """)
 
-    plan = pyhop2.find_plan(state1,[('travel','alice','park'),('travel','bob','park')],verbose=2)
+#     plan = pyhop2.find_plan(state1,[('travel','alice','park'),('travel','bob','park')],verbose=2)
 
-    check_result(plan,[('call_taxi', 'alice', 'home_a'), ('ride_taxi', 'alice', 'park'), ('pay_driver', 'alice', 'park'), ('walk', 'bob', 'home_b', 'park')])
+#     check_result(plan,[('call_taxi', 'alice', 'home_a'), ('ride_taxi', 'alice', 'park'), ('pay_driver', 'alice', 'park'), ('walk', 'bob', 'home_b', 'park')])
 
 
-    print("""
-Next, we'll use run_lazy_lookahead to try to get Alice to the park. With
-Pr = 1/2, the taxi won't arrive. In this case, run_lazy_lookahead will call
-find_plan again, and find_plan will return the same plan as before. This will
-happen repeatedly until either the taxi arrives or run_lazy_lookahead decides
-it has tried too many times.""")
-    pause()
+#     print("""
+# Next, we'll use run_lazy_lookahead to try to get Alice to the park. With
+# Pr = 1/2, the taxi won't arrive. In this case, run_lazy_lookahead will call
+# find_plan again, and find_plan will return the same plan as before. This will
+# happen repeatedly until either the taxi arrives or run_lazy_lookahead decides
+# it has tried too many times.""")
+#     pause()
 
-    new_state = pyhop2.run_lazy_lookahead(state1,[('travel','alice','park')],verbose=1)
+#     new_state = pyhop2.run_lazy_lookahead(state1,[('travel','alice','park')],verbose=1)
 
-    pause()
+#     pause()
 
-    print('')
-    print('If run_lazy_lookahead succeeded, then Alice is now at the park,')
-    print('so the planner will return an empty plan:\n')
+#     print('')
+#     print('If run_lazy_lookahead succeeded, then Alice is now at the park,')
+#     print('so the planner will return an empty plan:\n')
 
-    plan = pyhop2.find_plan(new_state,[('travel','alice','park')],verbose=1)
-    check_result(plan,[])
+#     plan = pyhop2.find_plan(new_state,[('travel','alice','park')],verbose=1)
+#     check_result(plan,[])
 
-    print("No more examples")
+#     print("No more examples")
 
 
 ###############################################################################
