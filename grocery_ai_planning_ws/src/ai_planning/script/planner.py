cc0de53d37ddf36d5b42911b844dfada53f3b1c7
Naman Gupta <namanryan@gmail.com>
2021-04-19 20:12:42 -0400

    Updated planner.py
    Test run for 2 items

--- grocery_ai_planning_ws/src/ai_planning/script/planner.py
+++ grocery_ai_planning_ws/src/ai_planning/script/planner.py
@@ -16,8 +16,6 @@ if __name__ == '__main__':
     node = rospy.init_node('a_star_planner')
     rospy.wait_for_service('follow_path')
     step_size = 0.4
-    start = (1.9, 5.59)
-    goal =(1.9, 7)
 
     try:
         rospy.set_param("turtlebot/step_size", step_size)
@@ -33,24 +31,33 @@ if __name__ == '__main__':
         pause()
         print("Use find plan to plan how to get Robot from start to the item.")
 
-        expected = [('move_robot', 'robot', 'item')]
+        expected = [('move_robot', 'robot', 'coliflower'), ('move_robot', 'robot', 'chicken')]
 
-        result = pyhop2.find_plan(state1, [('groceryshop','robot','item')],verbose=3)
+        result = pyhop2.find_plan(state1, [('groceryshop','robot','coliflower'), ('groceryshop','robot','chicken')],verbose=3)
         check_result(result, expected)
 
         # pause()
         # new_state = pyhop2.run_lazy_lookahead(state1,[('groceryshop','robot','item')],verbose=3)
-        # pause()
+        pause()
 
-        path = groceryplan.state0.wayp
+        total_path = groceryplan.state0.wayp
         
-        path_msg = []
-        for point in path:
-            waypoint = WayPoint()
-            waypoint.coord = point
-            path_msg.append(waypoint)
-        
-        resp = follow_path(path_msg)
+        item_ctr = 0
+
+        while(total_path):
+
+        	path = total_path.pop(0)
+
+	        path_msg = []
+	        for point in path:
+	            waypoint = WayPoint()
+	            waypoint.coord = point
+	            path_msg.append(waypoint)
+	        
+	        resp = follow_path(path_msg)
+
+	        item_ctr++
+	        print('Item No: ', item_ctr, 'picked!!')
 
         print(resp)
         
